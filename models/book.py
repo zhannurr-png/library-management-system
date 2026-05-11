78c690b9c3a48e01b66769aa16108b66471fc7a9
yerassel <asselermek007@gmail.com>
2026-05-11 11:41:44 +0500

    Update book.py

--- models/book.py
+++ models/book.py
@@ -16,3 +16,27 @@ class Book:
 
     def available(self):
         return self.available
+        
+    def borrow(self):
+        if not self.available:
+            return False
+        self.available = False
+        return True
+
+    def return_book(self):
+        self.available = True
+
+    def as_tuple(self):
+        return (self.book_id, self.title, self.author, self.available)
+
+    def to_dict(self):
+        return {
+            "id": self.book_id, "title": self.title,
+            "author": self.author, "available": self.available}
+
+    def __str__(self):
+        if self.available:
+            status = "available"
+        else:
+            status = "not available"
+        return f"[{self.book_id}] {self.title} by {self.author} ({status})"
