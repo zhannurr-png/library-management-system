d241c95ba07294ecb7a03ebbe32b9905792d3a7e
yerassel <asselermek007@gmail.com>
2026-05-11 14:58:30 +0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ class User:
         self.borrowed_books = []  
 
     def can_borrow(self):
-        return False    
+        return False      #subclasses (Member or Admin) implement can_borrow()
 
     def add_borrowed(self, book_id):
         self.borrowed_books.append(book_id)
@@ -19,3 +19,19 @@ class User:
 
     def __str__(self):
         return f"User: {self.name} (ID: {self.user_id}), borrowed: {self.borrowed_books}"
+
+class Member(User):
+    max_borrow = 3      #maximum number of book that can be borrowed
+
+    def can_borrow(self):
+        return len(self.borrowed_books) < self.max_borrow
+
+    def __str__(self):
+        return f"Member: {self.name} (ID: {self.user_id}), borrowed: {self.borrowed_books}"
+
+class Admin(User):
+    def can_borrow(self):
+        return True  # admin has no limit
+
+    def __str__(self):
+        return f"Admin: {self.name} (ID: {self.user_id}), borrowed: {self.borrowed_books}"
