fa738123b0bf10cb7e87f6d11d0f78624aab6e4c
ajym <fxynszrbpk@privaterelay.appleid.com>
2026-05-16 22:34:11 +0500

    import + init method

--- services/library_service.py
+++ services/library_service.py
@@ -11,30 +11,3 @@ class LibraryService:
         self.users = load_users()
         self.transactions = load_transactions()
 
-def borrow_book(self, user_id, book_id):
-    if user_id not in self.users:
-        raise ValueError("Invalid user_id.")
-    if book_id not in self.books:
-        raise ValueError("Invalid book_id.")
-
-    user = self.users[user_id]
-    book = self.books[book_id]
-
-    if not book.available:
-        raise ValueError(f"'{book.title}' is not available right now.")
-    if not user.can_borrow():
-        raise ValueError(f"'{user.name}' has reached the borrowing limit.")
-    if book_id in user.borrowed_books:
-        raise ValueError("You already borrowed this book")
-
-    book.borrow()
-    user.add_borrowed_book(book_id)
-
-    t = Transaction(user_id, book_id, "borrow")
-    self.transactions.append(t)
-
-    save_books(self.books)
-    save_users(self.users)
-    save_transactions(self.transactions)
-
-    print(f" {user.name} successfully borrowed book! '{book.title}'.")
