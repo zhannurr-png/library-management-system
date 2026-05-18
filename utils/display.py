862df456d36e116fda664ec9e131e530457c74d1
zhannurr-png <253750@astanait.edu.kz>
2026-05-18 09:50:31 +0500

    create user info display

--- utils/display.py
+++ utils/display.py
@@ -9,3 +9,8 @@ def print_book_list(books): #show all books in the list
         return
     for book in books: #print each book one by one
         print(f"{book}")
+
+def print_user_info(user): #user information display
+    print(f"Name: {user.name}")
+    print(f"ID: {user.user_id}")
+    print(f"Borrowed: {user.borrowed_books}")
