5b29b42840b6ea8d89edc173f8f678efa50b4f9d
zhannurr-png <253750@astanait.edu.kz>
2026-05-19 15:26:27 +0500

    fixed error

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from services.library_service import LibraryService, return_book
+from services.library_service import LibraryService
 from services.report import (report_available, report_borrowed,
     report_user_history)
 from utils.display import print_header, print_book_list, print_user_info
